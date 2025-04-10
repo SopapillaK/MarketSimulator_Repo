e5caf9f06d48b64129f0bed0dfb815fc542162eb
AnnabellemRuckle <rucklea@my.erau.edu>
2025-04-10 10:43:37 -0700

    vegetables

--- MarketSimulator_Project/Assets/ShopUIManager.cs
+++ MarketSimulator_Project/Assets/ShopUIManager.cs
@@ -4,30 +4,57 @@ using UnityEngine;
 public class ShopUIManager : MonoBehaviour
 {
     public Web web;
-    public string userID = "1"; //dynamically set
+    public string userID = "1"; //dynamically set after login
 
+    //fruits
     public void OnBuyApplePressed()
     {
-        StartCoroutine(web.BuyItem(userID, "1")); // Apples
+        StartCoroutine(web.BuyItem(userID, "1")); //apples
     }
 
     public void OnBuyPearPressed()
     {
-        StartCoroutine(web.BuyItem(userID, "2")); // Pears
+        StartCoroutine(web.BuyItem(userID, "2")); //pears
     }
 
     public void OnBuyBananaPressed()
     {
-        StartCoroutine(web.BuyItem(userID, "3")); // Bananas
+        StartCoroutine(web.BuyItem(userID, "3")); //bananas
     }
 
     public void OnBuyKiwiPressed()
     {
-        StartCoroutine(web.BuyItem(userID, "4")); // Kiwis
+        StartCoroutine(web.BuyItem(userID, "4")); //kiwis
     }
 
     public void OnBuyStrawberryPressed()
     {
-        StartCoroutine(web.BuyItem(userID, "5")); // Strawberries
+        StartCoroutine(web.BuyItem(userID, "5")); //strawberries
+    }
+
+    //vegetables
+    public void OnBuyPepperPressed()
+    {
+        StartCoroutine(web.BuyItem(userID, "6")); //peppers
+    }
+
+    public void OnBuyBroccoliPressed()
+    {
+        StartCoroutine(web.BuyItem(userID, "7")); //broccoli
+    }
+
+    public void OnBuyCeleryPressed()
+    {
+        StartCoroutine(web.BuyItem(userID, "8")); //celery
+    }
+
+    public void OnBuyLettucePressed()
+    {
+        StartCoroutine(web.BuyItem(userID, "9")); //lettuce
+    }
+
+    public void OnBuyCarrotPressed()
+    {
+        StartCoroutine(web.BuyItem(userID, "10")); //carrots
     }
 }
